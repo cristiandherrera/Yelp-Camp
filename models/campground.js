813284b32ff9f9279141179e5c7d5356905b49f1
Cristian Herrera <cristiandherrera95@gmail.com>
2021-10-20 19:52:33 -0700

    commiting project to github

---
+++ models/campground.js
@@ -0,0 +1,34 @@
+// SCHEMA SETUP
+// const mongoose = require("mongoose");
+
+// let campgroundSchema = new mongoose.Schema({
+// 	name: String,
+// 	image: String, 
+// 	description: String,
+// });
+
+// module.exports = mongoose.model("Campground", campgroundSchema);
+
+var mongoose = require("mongoose");
+ 
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   price: String,
+   image: String,
+   description: String,
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+ 
+module.exports = mongoose.model("Campground", campgroundSchema);
